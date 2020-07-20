63cd769364f89eb956084f5db21f8892a0e9687a
ayagaa <odwar235@gmail.com>
2020-07-20 15:38:25 +0300

    Add project files.

---
+++ BankingAPI/Controllers/BankingController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BankingAPI.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BankingController : ControllerBase
+    {
+        [HttpPost("createaccount")]
+        public async Task<ActionResult<string>> CreateAccount()
+        {
+
+            return NotFound(string.Empty);
+        }
+
+        [HttpPost("deleteaccount")]
+        public async Task<ActionResult<string>> DeleteAccount()
+        {
+            return NotFound(string.Empty);
+        }
+
+        [HttpPost("getbalance")]
+        public async Task<ActionResult<string>> GetBalance()
+        {
+            return NotFound(string.Empty);
+        }
+
+        [HttpPost("depositamount")]
+        public async Task<ActionResult<string>> DepositAmount()
+        {
+            return NotFound(string.Empty);
+        }
+
+        [HttpPost("withdrawamount")]
+        public async Task<ActionResult<string>> WithdrawAmount()
+        {
+            return NotFound(string.Empty);
+        }
+    }
+}
